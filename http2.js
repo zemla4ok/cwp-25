2780282994be290376cbc82d524fa865f81ea241
zemla4ok <dima-vital@live.com>
2018-03-14 12:06:26 +0300

    load dependencies

---
+++ http2.js
@@ -0,0 +1,23 @@
+const http2 = require('http2');
+const fs = require('fs');
+
+const key = fs.readFileSync('localhost.key');
+const cert = fs.readFileSync('localhost.cert');
+
+const server = http2.createSecureServer(
+  { key, cert },
+  onRequest
+);
+
+function onRequest(req, res) {
+  console.log(req.headers[':path']);
+
+  res.stream.respond({
+    'content-type': 'text/html',
+    ':status': 200
+  });
+
+  res.stream.end('<h1>Hello World</h1>');
+}
+
+server.listen(3000);
