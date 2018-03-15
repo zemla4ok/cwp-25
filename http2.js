590c9633bea3d01033d4bf6b84bd804c94aa16d5
zemla4ok <dima-vital@live.com>
2018-03-15 08:35:57 +0300

    task 3 completed

--- http2.js
+++ http2.js
@@ -1,8 +1,11 @@
 const http2 = require('http2');
 const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
 
 const key = fs.readFileSync('localhost.key');
 const cert = fs.readFileSync('localhost.cert');
+const {HTTP2_HEADER_PATH} = http2.constants;
 
 const server = http2.createSecureServer(
   { key, cert },
@@ -12,12 +15,47 @@ const server = http2.createSecureServer(
 function onRequest(req, res) {
   console.log(req.headers[':path']);
 
-  res.stream.respond({
+  const filePath = path.join('./public', req.headers[':path']);
+  if(req.headers[':path'] === '/index.html'){
+    push(req.stream, 'site.css');
+    push(req.stream, 'app.js');
+  }
+  req.stream.respondWithFile(filePath,
+                        {'content-type': mime.getType(filePath)});
+
+  /*res.stream.respond({
     'content-type': 'text/html',
     ':status': 200
-  });
+  });*/
 
-  res.stream.end('<h1>Hello World</h1>');
+
+  //res.stream.end('<h1>Hello World</h1>');
+}
+
+function push(stream, filePath) {
+  const fullResourcePath = path.join('./public', filePath);
+  const {descriptor, headers} = getFileInfo(fullResourcePath);
+  const pushHeaders = {[HTTP2_HEADER_PATH]: '/' + filePath};
+
+  stream.pushStream(pushHeaders, (err, pushStream) => {
+      pushStream.respondWithFD(descriptor, headers)
+  });
+}
+
+function getFileInfo(filePath) {
+  const descriptor = fs.openSync(filePath, 'r');
+
+  const stat = fs.fstatSync(descriptor);
+  const contentType = mime.getType(filePath);
+
+  return {
+      descriptor,
+      headers: {
+          'content-length': stat.size,
+          'last-modified': stat.mtime.toUTCString(),
+          'content-type': contentType
+      }
+  }
 }
 
 server.listen(8443, 'localhost', () => {
