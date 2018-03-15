ae9fa39a953dc6ec9cb4d4892ee11e7d5ce01ea9
zemla4ok <dima-vital@live.com>
2018-03-15 09:53:39 +0300

    compression added

--- http1.js
+++ http1.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const compression = require('compression');
+
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+app.use(compression({level: 6}));
 app.use('/', express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.send('Hello World!');
